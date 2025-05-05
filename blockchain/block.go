087034f5b9115626b72f704e80e9364da17cc4ea
gauravraj0510 <gaurav.rajghs@gmail.com>
2025-05-05 07:20:34 +0530

    Badger DB added with CLI functionalities

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,10 @@
 package blockchain
 
-
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 type Block struct {
 	Hash     []byte
@@ -12,9 +13,6 @@ type Block struct {
 	Nonce    int
 }
 
-// CreateBlock initializes a new block using the provided data and the previous block's hash.
-// It calculates the block's hash and nonce using the Proof of Work system,
-// and returns the newly created block.
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash, 0}
 	pow := NewProof(block)
@@ -26,22 +24,35 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-// AddBlock adds a new block to the BlockChain, given some data.
-// It uses the last block in the BlockChain as the previous block,
-// and appends the newly created block to the BlockChain.
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
-}
-
-// Genesis returns the Genesis block, which is the first block in any BlockChain.
-// The Genesis block has a data of "Genesis", and a previous hash of nil.
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-// InitBlockChain returns a new BlockChain containing the Genesis block.
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	Handle(err)
+
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- blockchain/proof.go
+++ blockchain/proof.go
@@ -28,8 +28,6 @@ type ProofOfWork struct {
 	Target *big.Int
 }
 
-// NewProof initializes a new ProofOfWork for the given block.
-// It calculates the target for the proof of work based on the predefined difficulty.
 func NewProof(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-Difficulty))
@@ -39,9 +37,6 @@ func NewProof(b *Block) *ProofOfWork {
 	return pow
 }
 
-// InitData takes the nonce and returns a slice of bytes that is a combination
-// of the previous block's hash, the data in the block, the nonce,
-// and the difficulty.
 func (pow *ProofOfWork) InitData(nonce int) []byte {
 	data := bytes.Join(
 		[][]byte{
@@ -56,9 +51,6 @@ func (pow *ProofOfWork) InitData(nonce int) []byte {
 	return data
 }
 
-// Run runs the Proof of Work algorithm. It starts with nonce equal to 0, and
-// increments the nonce until a hash is found that meets the requirements of the
-// Proof of Work. It returns the nonce and the hash.
 func (pow *ProofOfWork) Run() (int, []byte) {
 	var intHash big.Int
 	var hash [32]byte
@@ -84,8 +76,6 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	return nonce, hash[:]
 }
 
-// Validate checks to see if the nonce and hash stored in the ProofOfWork
-// satisfy the Proof of Work requirements.
 func (pow *ProofOfWork) Validate() bool {
 	var intHash big.Int
 
@@ -97,9 +87,6 @@ func (pow *ProofOfWork) Validate() bool {
 	return intHash.Cmp(pow.Target) == -1
 }
 
-// ToHex takes an int64 and converts it to a slice of bytes.
-// It does this by using the binary.Write function to write the int64 to a
-// bytes.Buffer in big-endian order.
 func ToHex(num int64) []byte {
 	buff := new(bytes.Buffer)
 	err := binary.Write(buff, binary.BigEndian, num)
